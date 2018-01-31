4d39491f7f83800b41a68a7a1bc3af3513692ded
ikukuvision <projects@ikukuvision.com>
2018-01-31 20:46:43 +0000

    First version of the APP before deploy

---
+++ meetup.py
@@ -0,0 +1,10 @@
+from APP import create_app, db, cli
+from APP.models import User, Post
+
+app = create_app()
+cli.register(app)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
